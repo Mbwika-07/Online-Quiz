a718fe425accf6bed229275ac743db67ab840289
Mbwika-07 <allanmbwiks@gmail.com>
2024-09-26 19:51:12 +0300

    .

--- teacher/forms.py
+++ teacher/forms.py
@@ -9,8 +9,6 @@ class TeacherUserForm(forms.ModelForm):
         widgets = {
         'password': forms.PasswordInput()
         }
-
-
                         
 class TeacherForm(forms.ModelForm):
     class Meta:
